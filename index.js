0f3f191766f171aa09c6a07bb74339fecea5892c
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-04-28 18:04:58 +0900

    linkbox first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function item_onmouseover() {
+    // if (item1) {
+    //     item1.innerText="심심해서 만들어 봄";
+    // }
+    // if (item2) {
+    //     item2.innerText="뉴에이지 곡이 좋아요";
+    // }
+    if (item3) {
+        item3.innerText="고려대 보건환경융합과학부 소개 영상";
+    }
+//     if (item4) {
+//         item4.innerText="djagpdud125@naver.com 바로 전송";
+//     }
+}
+
+function item_onmouseout() {
+    // if (item1) {
+    //     item1.innerText="코딩 공부 : 혜영네컷";
+    // }
+    // if (item2) {
+    //     item2.innerText="뉴에이지 곡이 좋아요";
+    // }
+    if (item3) {
+        item3.innerText="케이쿠 스튜디오 영상 제작";
+    }
+    // if (item4) {
+    //     item4.innerText="djagpdud125@naver.com 바로 전송";
+    // }
+}
